a051cc5fd29fc1fba84966823e6468c3e17f496f
omarashour222 <omarabdoulrahman@gmail.com>
2024-11-27 20:32:08 +0200

    custom appbar in home

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            Assets.logo,
+            height: 18,
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              CupertinoIcons.search,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
