dbb1dec6ae718e1369549d78de4ae68b3c44d705
omarashour222 <omarabdoulrahman@gmail.com>
2025-01-27 23:49:23 +0200

    open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main()async {
   runApp(const BooklyApp());
+
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class BooklyApp extends StatelessWidget {
@@ -22,5 +27,3 @@ class BooklyApp extends StatelessWidget {
     );
   }
 }
-
-
