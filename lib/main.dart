3ad2246939df50a537a8ee8416552d0694805150
omarashour222 <omarabdoulrahman@gmail.com>
2025-01-29 18:23:39 +0200

    create simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/functions/setup_service_locator.dart';
+import 'package:bookly_app/core/utils/simple_bloc_observer.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/use_cases/featch_featured_books_use_case.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_newest_use_case.dart';
+import 'package:bookly_app/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
-    await Hive.initFlutter();
-
+  await Hive.initFlutter();
+  setupServiceLocator();
   Hive.registerAdapter(BookEntityAdapter());
+
   await Hive.openBox<BookEntity>(kFeaturedBox);
-    await Hive.openBox<BookEntity>(kNewestBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const BooklyApp());
-
-
 }
 
 class BooklyApp extends StatelessWidget {
@@ -22,13 +30,32 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FeatchFeaturedBooksUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            FeatchNewestBooksUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          ),
+        )
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
